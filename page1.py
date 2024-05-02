2a3e44bdc8e3b584dacabdbca92b4869620aec70
fatimatwii <112896509+fatimatwii@users.noreply.github.com>
2024-05-02 12:07:36 +0300

    Add files via upload

---
+++ page1.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template, request
+import os
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/upload', methods=['POST'])
+def upload():
+    if 'audio_file' not in request.files:
+        return 'No file part'
+    
+    file = request.files['audio_file']
+    if file.filename == '':
+        return 'No selected file'
+    
+    if file:
+        file_path = os.path.join('uploads', file.filename)
+        file.save(file_path)
+        return render_template('index.html', file_path=file_path)
+
+if __name__ == '__main__':
+    app.run(debug=True)
