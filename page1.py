a6c017401a7f388697285bbd2857328d1230571a
fatimatwii <112896509+fatimatwii@users.noreply.github.com>
2024-05-12 13:13:29 +0300

    Update page1.py

--- page1.py
+++ page1.py
@@ -1,26 +1,49 @@
-from flask import Flask, render_template, request, url_for
+from flask import Flask, render_template, request, send_from_directory
 import os
 
 app = Flask(__name__)
 
+UPLOAD_FOLDER = 'upload'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+def get_next_record_number():
+    record_files = [file for file in os.listdir(app.config['UPLOAD_FOLDER']) if file.startswith('record')]
+    if record_files:
+        record_numbers = [int(file.split('.')[0].replace('record', '')) for file in record_files]
+        return max(record_numbers) + 1
+    else:
+        return 1
+
+record_count = get_next_record_number()
+
 @app.route('/')
-def home():
+def index():
     return render_template('index.html')
 
 @app.route('/upload', methods=['POST'])
-def upload(): 
-    if 'audio_file' not in request.files:
-        return render_template('index.html', file_path='No file part')
+def upload():
+    global record_count
     
-    file = request.files['audio_file']
-    if file.filename == '':
-     
-        return render_template('index.html', file_path='No selected file')
     
-    if file:
-        file_path = os.path.join('uploads', file.filename)
-        file.save(file_path)
-        return render_template('index.html', file_path=file_path)
+    uploaded_audio = request.files.get('uploaded_audio')
+    if uploaded_audio:
+        filename = f'upload_{uploaded_audio.filename}'
+        uploaded_audio.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        return render_template('index.html', file_path=filename)
+    
+    
+    recorded_audio = request.files.get('recorded_audio')
+    if recorded_audio:
+        filename = f'record{record_count}.wav'
+        recorded_audio.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        record_count += 1  
+        return render_template('index.html', file_path=filename)
+    
+    return render_template('index.html', file_path=None)
 
-if __name__ == '__main__':
+@app.route('/download/<filename>', methods=['GET'])
+def download(filename):
+    return send_from_directory(app.config['UPLOAD_FOLDER'], filename, as_attachment=True)
+
+if __name__ == "__main__":
     app.run(debug=True)
